07b18dcb491cda68319d2f358aac5d0b6872d9cd
jakimiszka <jarek.jakimicha@gmail.com>
2025-11-01 12:48:19 +0100

    sript in progress

---
+++ script.js
@@ -0,0 +1,14 @@
+const mobileNavbar = document.querySelector('.mobile_navbar');
+const openMenuBtn = document.querySelector('.navbar_hamburger');
+const closeMenuBtn = document.querySelector('.mobile_navbar_close');
+const overlay = document.querySelector('.overlay');
+
+openMenuBtn.addEventListener('click', () => {
+    mobileNavbar.style.display = 'block';
+    overlay.style.visibility = 'visible';
+});
+
+closeMenuBtn.addEventListener('click', () => {
+    mobileNavbar.style.display = 'none';
+    overlay.style.visibility = 'hidden';
+});
