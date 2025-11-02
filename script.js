c4b57b95c07ef2912b34dc1d083278b8b016f6b4
jakimiszka <jarek.jakimicha@gmail.com>
2025-11-02 11:05:31 +0100

    navbar reused for mobile

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const openMenuBtn = document.querySelector('.navbar_hamburger');
 const closeMenuBtn = document.querySelector('.mobile_navbar_close');
 const overlay = document.querySelector('.overlay');
 
-openMenuBtn.addEventListener('click', () => {
-    mobileNavbar.style.display = 'block';
-    overlay.style.visibility = 'visible';
-});
+// openMenuBtn.addEventListener('click', () => {
+//     mobileNavbar.style.display = 'block';
+//     overlay.style.visibility = 'visible';
+// });
 
-closeMenuBtn.addEventListener('click', () => {
-    mobileNavbar.style.display = 'none';
-    overlay.style.visibility = 'hidden';
-});
+// closeMenuBtn.addEventListener('click', () => {
+//     mobileNavbar.style.display = 'none';
+//     overlay.style.visibility = 'hidden';
+// });
